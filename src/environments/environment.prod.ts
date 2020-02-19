a3f207a6bcc433dea57b5eecb650706c6c8a2a45
parul12344 <parulbhusri44@gmail.com>
2020-02-19 17:15:12 +0530

    made some changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+    apiKey: '069a60563a31c8c8d052f3c37b3b5110',
+   apiUrl: 'http://api.openweathermap.org/data/2.5',
+   jsonFileURL: 'assets/city.json'
 };
+
